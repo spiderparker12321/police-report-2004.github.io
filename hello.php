e114064cc929f40a336d9090239909c20be4e76a
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-03 10:34:28 -0700

    Create hello.php

---
+++ hello.php
@@ -0,0 +1,15 @@
+<html>
+<body>
+
+
+    
+  <center><h1>Police report</h1></center>
+  <a href="https://www.youtube.com/results?search_query=how+to+add+a+working+pasword+html">link text</a>
+  
+<form class="FormInputArea">
+  <textarea class="InputCamp" id="TextArea" placeholder="Explain your move in max 200 characters..." required></textarea>
+  <button class="button" type="submit" id="SendButton"> Send </button>
+</form>
+    
+</body>
+</html>
