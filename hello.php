0650131cf6cb3a1e1181c5a57f1af5388b0352f8
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-04 09:49:45 -0700

    Update hello.php

--- hello.php
+++ hello.php
@@ -1,17 +1,17 @@
 <?php
 // Password Validator
 
-//$length =false;
-//$upper =false;
-//$lower=false;
-//$num=false;
-//$sym_in =false;
-//$sym_out=true;
+$length =false;
+$upper =false;
+$lower=false;
+$num=false;
+$sym_in =false;
+$sym_out=true;
 $right_password = false;
 
-//$message = '';
-//$in_ar = ["!","%","-","_"];
-//$out_ar=["@","$","."];
+$message = '';
+$in_ar = ["!","%","-","_"];
+$out_ar=["@","$","."];
 
 echo "Enter password: ";
 $pswd = fgets(STDIN);
