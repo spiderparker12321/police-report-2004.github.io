bb0889be246049029caf042f5e479f900de31c45
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-05 10:59:51 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -3,7 +3,7 @@ var password = document.getElementById("password").value;
 var confirmPassword = document.getElementById("confirm_password").value;
 
 if(password == hello){
-
+window.open("https://www.w3schools.com");
 }
 else{
 alert("Password don't match!");
