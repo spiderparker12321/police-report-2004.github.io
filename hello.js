98a4e481b040bef30d9d0ca974e92d4134c98d70
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-05 10:22:49 -0700

    Update hello.js

--- hello.js
+++ hello.js
@@ -2,8 +2,8 @@ var password = "hello";
 var response;
 var start;
 
-for(start){
-if (response == password){
-window.location.replace("http://www.w3schools.com");
+while(response != password){
 }
+window.location.replace("http://www.w3schools.com");
+
 
