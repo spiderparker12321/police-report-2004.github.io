71b332e7a44a3dfe322b625ce4028d1ae2979b96
spiderparker12321 <149962198+spiderparker12321@users.noreply.github.com>
2024-12-05 12:59:40 -0700

    Update hey.js

--- hey.js
+++ hey.js
@@ -3,5 +3,6 @@ var responce;
 
 while(responce != password){
 responce = window.prompt("enter password");
+window.location.replace("https://spiderparker12321.github.io/police-report-2004.github.io/stop.html");
 }
 window.location.replace("https://spiderparker12321.github.io/police-report-2004.github.io/stop.html");
